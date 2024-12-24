6256e1c920977b2a92a2314f41e143f124a0e50a
Martin Musiol <martin.musiol@tum.de>
2024-12-24 14:46:35 +0100

    Added local image and updated templates to use it

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def home():
